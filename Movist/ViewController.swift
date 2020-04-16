1e2efb7d25eb72aad4276374136896e8a12d48af
yildizkoray <koray.yildizz@gmail.com>
2020-04-16 21:24:16 +0300

    Initial Commit

---
+++ Movist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movist
+//
+//  Created by Yildiz, Koray(AWF) on 16.04.2020.
+//  Copyright © 2020 Koray Yildiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

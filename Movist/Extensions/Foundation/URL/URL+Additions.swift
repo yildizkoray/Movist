94127e769a766043c60098f5f773c366ba2fadff
yildizkoray <koray.yildizz@gmail.com>
2020-05-12 01:45:07 +0300

    Refactor URL Extensions

--- Movist/Extensions/Foundation/URL/URL+Additions.swift
+++ Movist/Extensions/Foundation/URL/URL+Additions.swift
@@ -8,17 +8,36 @@
 
 import Foundation
 
+// MARK: - URL+Endpoints
+
 public extension URL {
     
     private static let RANDOM_IMAGE_BASE_URL = URL(string: ConfigReader.shared["RANDOM_IMAGE_URL"])!
     static let TMDB_API_URL = URL(string: ConfigReader.shared["TMDB_API_URL"])!
     static let TMDB_IMAGE_URL = URL(string: ConfigReader.shared["TMDB_IMAGE_URL"])!
+}
+
+// MARK: - URL+Images
+
+public extension URL {
     
-    static func randomIcon(width: Int, height: Int) -> URL {
+    static func backdrop(for config: BackdropSize = .w300, file: String) -> URL {
+        let path: String = "\(config.rawValue)/\(file)"
+        return URL(string: path, relativeTo: .TMDB_IMAGE_URL)!
+    }
+    
+    static func poster(for config: PosterSize = .w342, file: String) -> URL {
+        let path: String = "\(config.rawValue)\(file)"
+        return URL(string: path, relativeTo: .TMDB_IMAGE_URL)!
+    }
+    
+    static func randomIcon(width: Int = 200, height: Int = 200) -> URL {
         let path: String = "\(width)/\(height)"
         return URL(string: path, relativeTo: .RANDOM_IMAGE_BASE_URL)!
     }
 }
+
+// MARK: - URL+VisualContent
 
 public extension Optional where Wrapped == URL {
     

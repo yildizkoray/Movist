33e4c0e8580a3cff4deb3c6c88fb1f9581d5de20
yildizkoray <koray.yildizz@gmail.com>
2020-06-21 16:21:23 +0300

    Add Review Field into MovieDetailDisplay

--- Movist/Scenes/Movie/MovieDetail/UI/Display/MovieDetailDisplay.swift
+++ Movist/Scenes/Movie/MovieDetail/UI/Display/MovieDetailDisplay.swift
@@ -12,12 +12,14 @@ public struct MovieDetailDisplay {
     let header: MovieDetailHeaderDisplay
     let casts: [CastDisplay]
     let similars: [MovieItemDisplay]
+    let reviews: [ReviewDisplay]
     
     public init(with movie: Movie) {
         title = movie.title.emptyIfNil
         header = MovieDetailHeaderDisplay.init(with: movie)
         casts = movie.cast.ifNil(.empty()).filter { $0.poster.emptyIfNil.isNotEmpty }.map(CastDisplay.init)
         similars = movie.similars.ifNil(.empty()).map(MovieItemDisplay.init)
+        reviews = movie.reviews.ifNil(.empty()).map(ReviewDisplay.init)
     }
     
     private init() {
@@ -25,6 +27,7 @@ public struct MovieDetailDisplay {
         header = .empty
         casts = .empty()
         similars = .empty()
+        reviews = .empty()
     }
     
     public static let empty = MovieDetailDisplay()
